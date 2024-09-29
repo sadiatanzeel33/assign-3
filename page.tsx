4141bda22dc39c59c4a95a1105921aad30608b30
sadiatanzeel33 <sadiatanzeel33@gmail.com>
2024-09-29 21:52:45 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
 
-const ContactPage = () => {
+const AboutPage = () => {
     return (
         <div>
-            <h1> This is ContactPage </h1>
-            <h2>https://www.facebook.com/Admission.org</h2>
-            <h2>call : 032222222222</h2>
-            <Link href='/'>Go to about home page.</Link>
-           
-           
+            <h1>This is about page</h1>
+         
+
+            <Link href='/'>Go to Home Page</Link>
             </div>
     )
 }
 
-export default ContactPage
+export default AboutPage
