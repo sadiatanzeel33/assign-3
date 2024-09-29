94c0895d76a3b73943b6c5168f629d8b1d4c160e
sadiatanzeel33 <sadiatanzeel33@gmail.com>
2024-09-29 21:50:37 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1> This is ContactPage </h1>
+            <h2>https://www.facebook.com/Admission.org</h2>
+            <h2>call : 032222222222</h2>
+            <Link href='/'>Go to about home page.</Link>
+           
+           
+            </div>
+    )
+}
+
+export default ContactPage
